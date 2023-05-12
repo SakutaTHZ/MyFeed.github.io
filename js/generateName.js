e23530e586f67e3d06fefed454384729c86d5687
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2023-05-12 23:25:41 +0630

    Feed Done (50%)
    - added generateName.js
       - generate UserName
       - generate Comments
       - generate background color
         (light)
    - added Intersection Observer for smooth animation

---
+++ js/generateName.js
@@ -0,0 +1,22 @@
+var adjective = ["Excited", "Anxious", "Overweight", "Demonic", "Jumpy", "Misunderstood", "Squashed", "Gargantuan","Broad", "Crooked", "Curved", "Deep", "Even","Excited", "Anxious", "Overweight", "Demonic", "Jumpy", "Misunderstood", "Squashed", "Gargantuan","Broad", "Crooked", "Curved", "Deep", "Even", "Flat", "Hilly", "Jagged", "Round", "Shallow", "Square", "Steep", "Straight", "Thick", "Thin", "Cooing", "Deafening", "Faint", "Harsh", "High-pitched", "Hissing", "Hushed", "Husky", "Loud", "Melodic", "Moaning", "Mute", "Noisy", "Purring", "Quiet", "Raspy", "Screeching", "Shrill", "Silent", "Soft", "Squeaky", "Squealing", "Thundering", "Voiceless", "Whispering"] 
+var object = ["Taco", "Operating System", "Sphere", "Watermelon", "Cheeseburger", "Apple Pie", "Spider", "Dragon", "Remote Control", "Soda", "Barbie Doll", "Watch", "Purple Pen", "Dollar Bill", "Stuffed Animal", "Hair Clip", "Sunglasses", "T-shirt", "Purse", "Towel", "Hat", "Camera", "Hand Sanitizer Bottle", "Photo", "Dog Bone", "Hair Brush", "Birthday Card", "Dude", "Child", "Man", "Woman", "Person", "Karen"]
+var verb = ["hit","ate","built","drew","threw","squashed","kiked","punched","fucked","licked","smashed"]
+var verb3 = ["hit","eaten","built","drawn","thrown","squashed","kiked","punched","fucked","licked","smashed"]
+var list;
+
+function generator() {
+ return adjective[Math.floor(Math.random() * adjective.length)] + " " + object[Math.floor(Math.random() * object.length)];;;
+}
+
+function generateComment(){
+    return Math.floor(Math.random() * 2) == 0 ? "'" + generator() + "' " + verb[Math.floor(Math.random() * verb.length)] + " '"+ generator() + "'" : "'" + generator() + "' was " + verb3[Math.floor(Math.random() * verb3.length)] + " by '"+ generator() + "'"
+}
+
+function getRandomColor() {
+    var letters = 'BCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,153 @@
-$(document).ready(function(){$(".post-image").click(function(){this.requestFullscreen()})});
+window.onload = () =>{
+    for (let index = 0; index < 3; index++) {
+        console.log('Loading Content');
+        generatePosts()
+    }
+}
+
+$(document).ready(function () { $(".post-image").click(function () { this.requestFullscreen() }) });
+
+// Generate random Posts
+function generatePosts() {
+	let postOwner = generator();
+	let commentOwner = generator();
+	let postedTime = Math.floor(Math.random() * 60);
+    let parent = document.querySelector('.post-section');
+    let child = document.createElement('div');
+	observer.observe(child);
+    child.className='post';
+	// child.classList.add('show');
+    child.innerHTML = `
+            <div class="post-head">
+				<div class="post-user-info">
+					<img class="profile" src="https://api.dicebear.com/6.x/notionists/svg?seed=${postOwner}" alt="profile">
+					<span class="active no"></span>
+					<div class="post-username-box">
+						<p>${postOwner}</p>
+						<p>${postedTime} mins ago</p>
+					</div>
+				</div>
+				<div class="post-option">
+					<span class="material-symbols-outlined">
+						more_horiz
+					</span>
+
+					<div class="post-option-box">
+						<p>
+							<span class="material-symbols-outlined">
+								star
+							</span>
+							Star
+						</p>
+						<p>
+							<span class="material-symbols-outlined">
+								download
+							</span>
+							Download
+						</p>
+						<p>
+							<span class="material-symbols-outlined">
+								report
+							</span>
+							Report
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="post-body">
+				<p>${generateComment()}</p>
+				<img style="--iColor:${getRandomColor()};" class="post-image" src="https://api.dicebear.com/6.x/notionists/svg?seed=${postOwner}" alt="profile">
+			</div>
+
+			<div class="post-foot">
+				<button>
+					<span class="material-symbols-outlined">
+						favorite
+					</span>
+					<span class="like-count">
+						${Math.floor(Math.random() * 101)}
+					</span>
+				</button>
+				<div class="comment-button">
+					<div class="comment-box">
+						<textarea name="" id="" cols="46" rows="2" placeholder="Comment Here"></textarea>
+						<button>
+							<span class="material-symbols-outlined">
+								send
+							</span>
+						</button>
+					</div>
+					<span class="material-symbols-outlined">
+						chat_bubble
+					</span>
+					<span class="comment-count">
+						${Math.floor(Math.random() * 50)}
+					</span>
+				</div>
+				<button>
+					<span class="material-symbols-outlined">
+						share
+					</span>
+					<span class="share-count">
+						${Math.floor(Math.random() * 10)}
+					</span>
+				</button>
+			</div>
+
+			<div class="comments">
+				<div class="comment">
+					<div class="comment-username-box">
+						<img class="profile" src="https://api.dicebear.com/6.x/notionists/svg?seed=${commentOwner}" alt="profile">
+						<div class="post-username-box">
+							<p>${commentOwner}</p>
+							<p>${Math.floor(Math.random() * postedTime)} mins ago</p>
+						</div>
+					</div>
+					<p class="comment-content">${generateComment()}</p>
+				</div>
+				<button class="seemore">See more ...</button>
+			</div>
+    `
+    parent.append(child);
+
+	posts = document.querySelectorAll(".post");
+}
+
+let posts = document.querySelectorAll(".post");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("show", entry.isIntersecting)
+            // if (entry.isIntersecting) observer.unobserve(entry.target)
+        })
+    },
+    {
+        rootMargin: "-40px"
+    }
+)
+
+const lastPostObserver = new IntersectionObserver(
+	entries => {
+		const lastPost = entries[0]
+		if(!lastPost.isIntersecting) return 
+		loadNewPosts()
+		lastPostObserver.unobserve(lastPost.target)
+		lastPostObserver.observe(document.querySelector(".post:last-child"))
+	},{}
+)
+
+setTimeout(() => {
+	lastPostObserver.observe(document.querySelector(".post:last-child"))
+}, 100);
+
+function loadNewPosts(){
+	for (let index = 0; index < 5; index++) {
+		generatePosts();
+	}
+}
+
+posts.forEach(post => {
+	observer.observe(post);
+})
